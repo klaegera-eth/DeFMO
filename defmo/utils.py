aee4e4acf802f6a228902972426bd20af1c0cb81
Adrian Klaeger <klaegera@student.ethz.ch>
2021-03-23 23:53:34 +0100

    zip individual comments and more

--- defmo/utils.py
+++ defmo/utils.py
@@ -8,22 +8,14 @@ from contextlib import contextmanager
 
 
 class ZipLoader:
-    def __init__(self, zip, filter="*[!/]", balance_subdirs=False):
+    def __init__(self, zip, filter="*[!/]"):
+        print("Loading", zip)
         self.zip = ZipFile(zip)
         self.names = fnmatch.filter(self.zip.namelist(), filter)
-        self.tree = None
-        if balance_subdirs:
-            # create directory tree of zip contents
-            dict_tree = lambda: defaultdict(dict_tree)
-            self.tree = dict_tree()
-            for name in self.names:
-                node = self.tree
-                for d in name.split("/")[:-1]:
-                    node = node[d]
-                node[name] = None
+        self.dirtree = None
 
     @contextmanager
-    def get_path(self, name):
+    def as_tempfile(self, name):
         _, ext = os.path.splitext(name)
         fd, path = tempfile.mkstemp(suffix=ext)
         with os.fdopen(fd, "wb") as f:
@@ -33,14 +25,23 @@ class ZipLoader:
         finally:
             os.remove(path)
 
-    def get_random_path(self):
-        if self.tree:
+    def get_random(self, balance_subdirs=False):
+        if balance_subdirs:
+            if not self.dirtree:
+                # create directory tree of zip contents
+                dict_tree = lambda: defaultdict(dict_tree)
+                self.dirtree = dict_tree()
+                for name in self.names:
+                    node = self.dirtree
+                    for d in name.split("/")[:-1]:
+                        node = node[d]
+                    node[name] = None
             # randomly sample at every level of directory tree
-            node = self.tree
+            node = self.dirtree
             while True:
                 name = random.choice(list(node.keys()))
                 node = node[name]
                 if not node:
                     # leaf node
-                    return self.get_path(name)
-        return self.get_path(random.choice(self.names))
+                    return name
+        return random.choice(self.names)
--- render_zip.py
+++ render_zip.py
@@ -18,7 +18,7 @@ args = render.ensure_blender(r"C:\Program Files\Blender Foundation\Blender 2.91\
 n_sequences = int(args[0]) if len(args) else 5
 out_dir = args[1] if len(args) > 1 else "."
 
-objs = utils.ZipLoader("data/ShapeNetCore.v2.zip", "*.obj", balance_subdirs=True)
+objs = utils.ZipLoader("data/ShapeNetCore.v2.zip", "*.obj")
 texs = utils.ZipLoader("data/textures.zip", "*/textures_train/*.jpg")
 
 p = dict(
@@ -36,25 +36,24 @@ render.init(p["n_frames"], p["resolution"])
 frustum = render.Frustum(p["z_range"], p["resolution"])
 
 time = datetime.now()
-filename = time.strftime(f"{len(p['blurs'])}-{p['n_frames']}_%y%m%d%H%M%S.zip")
+filename = time.strftime(f"fmo_{len(p['blurs'])}_{p['n_frames']}_%y%m%d%H%M%S.zip")
 
 with zipfile.ZipFile(os.path.join(out_dir, filename), "w") as zip:
     zip.comment = json.dumps(p).encode()
     for i in range(n_sequences):
+        obj = objs.get_random(balance_subdirs=True)
+        tex = texs.get_random()
+        loc = frustum.gen_point_pair(p["delta_z"], p["delta_xy"])
         rot_start = np.random.rand(3) * 2 * np.pi
         rot_end = rot_start + p["max_rot"] * (np.random.rand(3) * 2 - 1)
-        with zip.open(f"{i:04}.webp", "w") as out:
-            with objs.get_random_path() as obj, texs.get_random_path() as tex:
-                render.render(
-                    out,
-                    obj,
-                    tex,
-                    frustum.gen_point_pair(p["delta_z"], p["delta_xy"]),
-                    (rot_start, rot_end),
-                    p["blurs"],
-                )
+        name = f"{i:04}.webp"
+        with objs.as_tempfile(obj) as objf, texs.as_tempfile(tex) as texf:
+            with zip.open(name, "w") as out:
+                render.render(out, objf, texf, loc, (rot_start, rot_end), p["blurs"])
+        zip.getinfo(name).comment = json.dumps({"obj": obj, "tex": tex}).encode()
 
 duration = datetime.now() - time
 print(
-    f"Rendered {n_sequences} sequences in {duration}, {duration.total_seconds() / n_sequences:.2f}s per sequence"
+    f"Rendered {n_sequences} sequences in {duration}, "
+    f"{duration.total_seconds() / n_sequences:.2f}s per sequence"
 )
