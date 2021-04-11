c9d77e8c403ea2f92388c4b5e93ad6aa118ea96f
Adrian Klaeger <klaegera@student.ethz.ch>
2021-04-11 02:22:46 +0200

    add load_image to ZipLoader

--- defmo/utils.py
+++ defmo/utils.py
@@ -3,6 +3,7 @@ import sys
 import random
 import fnmatch
 import tempfile
+from PIL import Image
 from zipfile import ZipFile
 from collections import defaultdict
 from contextlib import contextmanager
@@ -17,35 +18,47 @@ def print_stderr(*args, **kwargs):
 
 class ZipLoader:
     def __init__(self, zip, filter="*[!/]", balance_subdirs=False):
-        self.zip = ZipFile(zip)
-        self.names = fnmatch.filter(self.zip.namelist(), filter)
-        self.dirtree = None
+        self._zip = ZipFile(zip)
+        self.names = fnmatch.filter(self._zip.namelist(), filter)
+        self._dirtree = None
 
         if balance_subdirs:
             # create directory tree of zip contents
             dict_tree = lambda: defaultdict(dict_tree)
-            self.dirtree = dict_tree()
+            self._dirtree = dict_tree()
             for name in self.names:
-                node = self.dirtree
+                node = self._dirtree
                 for d in name.split("/")[:-1]:
                     node = node[d]
                 node[name] = None
+
+    def __len__(self):
+        return len(self.names)
+
+    def open(self, name):
+        return self._zip.open(name)
+
+    def load_image(self, name):
+        with self.open(name) as f:
+            img = Image.open(f)
+            img.load()
+            return img
 
     @contextmanager
     def as_tempfile(self, name):
         _, ext = os.path.splitext(name)
         fd, path = tempfile.mkstemp(suffix=ext)
         with os.fdopen(fd, "wb") as f:
-            f.write(self.zip.read(name))
+            f.write(self._zip.read(name))
         try:
             yield path
         finally:
             os.remove(path)
 
     def get_random(self):
-        if self.dirtree:
+        if self._dirtree:
             # randomly sample at every level of directory tree
-            node = self.dirtree
+            node = self._dirtree
             while True:
                 name = random.choice(list(node.keys()))
                 node = node[name]
@@ -57,7 +70,7 @@ class ZipLoader:
     def get_random_seq(self, length):
         for _ in range(10000):
             seed = self.get_random()
-            node = self.dirtree
+            node = self._dirtree
             for d in seed.split("/")[:-1]:
                 node = node[d]
             names = sorted(node.keys())
