028cd1120adc757fc9c91d0ae0dd93007e0b54af
Adrian Klaeger <klaegera@student.ethz.ch>
2021-06-11 21:44:03 +0200

    add comparison losses

--- defmo/lightning/module.py
+++ defmo/lightning/module.py
@@ -7,12 +7,13 @@ import defmo.training.modules as mod
 
 
 class DeFMO(pl.LightningModule):
-    def __init__(self, encoder, renderer, losses):
+    def __init__(self, encoder, renderer, losses, comparison_losses=[]):
         super().__init__()
         self.save_hyperparameters()
         self.encoder = mod.Encoder(encoder)
         self.renderer = mod.Renderer(renderer)
         self.loss = mod.Loss(losses)
+        self.comparison_loss = mod.Loss(comparison_losses)
 
     @classmethod
     def add_model_specific_args(cls, parser, required=True):
@@ -20,10 +21,17 @@ class DeFMO(pl.LightningModule):
         group.add_argument("--encoder", required=required)
         group.add_argument("--renderer", required=required)
         group.add_argument("--losses", nargs="+", required=required)
+        group.add_argument("--comparison-losses", nargs="+", default=[])
 
     @classmethod
     def from_args(cls, args, **kwargs):
-        return cls(args.encoder, args.renderer, args.losses, **kwargs)
+        return cls(
+            args.encoder,
+            args.renderer,
+            args.losses,
+            args.comparison_losses,
+            **kwargs,
+        )
 
     def on_train_start(self):
         self.logger.log_hyperparams(self.hparams, {"valid_loss": float("inf")})
@@ -37,22 +45,29 @@ class DeFMO(pl.LightningModule):
         renders = self.renderer(latent, n_frames)
         return dict(latent=latent, renders=renders)
 
-    def step(self, inputs):
+    def step(self, inputs, log_name):
         outputs = self(inputs)
-        return self.loss(inputs, outputs).mean(0), outputs
+        loss = self.loss(inputs, outputs).mean(0)
+
+        self.loss.log(log_name, loss, self.log, sync_dist=True)
+        loss = loss.mean()
+        self.log(log_name, loss, sync_dist=True)
+
+        with torch.no_grad():
+            closs = self.comparison_loss(inputs, outputs).mean(0)
+            self.comparison_loss.log(
+                log_name, closs, self.log, sync_dist=True, prog_bar=True
+            )
+
+        return loss, outputs
 
     def training_step(self, inputs, _):
-        loss, _ = self.step(inputs)
-        self.loss.log("train_loss", loss, self.log, sync_dist=True)
-        loss = loss.mean()
-        self.log("train_loss", loss, sync_dist=True)
+        loss, _ = self.step(inputs, "train_loss")
+
         return loss
 
     def validation_step(self, inputs, batch_idx):
-        loss, outputs = self.step(inputs)
-        self.loss.log("valid_loss", loss, self.log, sync_dist=True)
-        loss = loss.mean()
-        self.log("valid_loss", loss, sync_dist=True)
+        loss, outputs = self.step(inputs, "valid_loss")
 
         if batch_idx == 0:
             self._log_gt_vs_renders(gt=inputs["frames"], renders=outputs["renders"])
