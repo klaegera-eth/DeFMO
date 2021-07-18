0e409270f43a9a32174565235a18dd3ad7a10220
Adrian Klaeger <klaegera@student.ethz.ch>
2021-07-18 15:58:29 +0200

    add single encoders

--- defmo/training/modules/encoder.py
+++ defmo/training/modules/encoder.py
@@ -2,6 +2,8 @@ import torch.nn as nn
 
 from torchvision.models import resnet50
 
+from .utils import group_norm
+
 
 class Encoder(nn.Module):
     def __init__(self, model, **kwargs):
@@ -12,43 +14,70 @@ class Encoder(nn.Module):
         return self.model(inputs)
 
     def models(_, name, **kwargs):
-        def _resnet_norm(norm_layer):
-            model = resnet50(norm_layer=norm_layer)
-            model.load_state_dict(
-                resnet50(pretrained=True).state_dict(),
-                strict=False,
-            )
-            return model
-
-        def _resnet_nomaxpool(resnet):
-            conv = nn.Conv2d(6, 64, kernel_size=7, stride=2, padding=3, bias=False)
-            conv.load_state_dict({"weight": resnet.conv1.weight.repeat(1, 2, 1, 1)})
-
-            return nn.Sequential(
-                Encoder.ImgsToChannels(),
-                conv,
-                resnet.bn1,
-                resnet.relu,
-                resnet.layer1,
-                resnet.layer2,
-                resnet.layer3,
-                resnet.layer4,
-            )
-
         def resnet_gn_nomaxpool():
-            from .utils import group_norm
+            resnet = Encoder.resnet_with_norm(group_norm())
+            return Encoder.resnet_double_input(resnet, maxpool=False)
 
-            resnet = _resnet_norm(group_norm())
-            return _resnet_nomaxpool(resnet)
+        def resnet_gn():
+            resnet = Encoder.resnet_with_norm(group_norm())
+            return Encoder.resnet_double_input(resnet)
+
+        def resnet_gn_single_nomaxpool():
+            resnet = Encoder.resnet_with_norm(group_norm())
+            return Encoder.resnet_single(resnet, maxpool=False)
+
+        def resnet_gn_single():
+            resnet = Encoder.resnet_with_norm(group_norm())
+            return Encoder.resnet_single(resnet)
 
         def v2():
             resnet = resnet50(pretrained=True)
-            return _resnet_nomaxpool(resnet)
+            return Encoder.resnet_double_input(resnet, maxpool=False)
 
         try:
             return locals()[name](**kwargs)
         except KeyError:
             raise ValueError(f"Encoder model '{name}' not found.")
+
+    @staticmethod
+    def resnet_with_norm(norm_layer):
+        model = resnet50(norm_layer=norm_layer)
+        model.load_state_dict(
+            resnet50(pretrained=True).state_dict(),
+            strict=False,
+        )
+        return model
+
+    @staticmethod
+    def resnet_double_input(resnet, maxpool=True):
+        conv = nn.Conv2d(6, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        conv.load_state_dict({"weight": resnet.conv1.weight.repeat(1, 2, 1, 1)})
+
+        return nn.Sequential(
+            Encoder.ImgsToChannels(),
+            conv,
+            resnet.bn1,
+            resnet.relu,
+            resnet.maxpool if maxpool else nn.Identity(),
+            resnet.layer1,
+            resnet.layer2,
+            resnet.layer3,
+            resnet.layer4,
+        )
+
+    @staticmethod
+    def resnet_single(resnet, maxpool=True):
+        return nn.Sequential(
+            Encoder.ImgsToChannels(),
+            resnet.conv1,
+            resnet.bn1,
+            resnet.relu,
+            resnet.maxpool if maxpool else nn.Identity(),
+            resnet.layer1,
+            resnet.layer2,
+            resnet.layer3,
+            resnet.layer4,
+        )
 
     class ImgsToChannels(nn.Module):
         def forward(self, imgs):
