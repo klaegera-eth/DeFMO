0ceee853bf8ffa73f11ae2e707844d09d479dd21
Adrian Klaeger <klaegera@student.ethz.ch>
2021-07-18 15:38:12 +0200

    add single_blur dataset

--- datasets.py
+++ datasets.py
@@ -2,41 +2,59 @@ from defmo.training.data import DataModule, MultiDataset, BackgroundAdder
 from defmo.utils import FmoLoader, ZipLoader
 
 
-def get_dataset(name, num_workers=1, **kwargs):
+def get_dataset(name, **kwargs):
     def double_blur(**kwargs):
-        return multi_object(n_obj=1, **kwargs)
+        return _dataset(**dict(blurs=[1, 2]) | kwargs)
 
-    def multi_object(train_range=(0, 0.9), valid_range=(0.9, 1), n_obj=[1, 2, 3]):
-        return DataModule(
-            train_data=MultiDataset(
-                FmoLoader("data/fmo_3_24_v1.zip", item_range=train_range, blurs=[1, 2]),
-                BackgroundAdder(
-                    ZipLoader("data/vot2018.zip", balance_subdirs=True),
-                ),
-                num_objects=n_obj,
-            ),
-            valid_data=MultiDataset(
-                FmoLoader("data/fmo_3_24_v1.zip", item_range=valid_range, blurs=[1, 2]),
-                BackgroundAdder(
-                    ZipLoader("data/otb.zip", filter="*.jpg", balance_subdirs=True),
-                ),
-                num_objects=n_obj,
-            ),
-            predict_data=MultiDataset(
-                FmoLoader("data/falling.zip", dummy_frames=24),
-                background_adder=None,
-            ),
-            num_workers=num_workers,
-        )
+    def multi_object(**kwargs):
+        return double_blur(**dict(num_objects=[1, 2, 3]) | kwargs)
 
-    def test():
-        return double_blur(train_range=(0, 9), valid_range=(9, 57))
+    def test(**kwargs):
+        return double_blur(**dict(train_range=(0, 9), valid_range=(9, 57)) | kwargs)
+
+    def single_blur(**kwargs):
+        return _dataset(**dict(blurs=[0]) | kwargs)
+
+    def test_single(**kwargs):
+        return single_blur(**dict(train_range=(0, 9), valid_range=(9, 57)) | kwargs)
 
     try:
-        if num_workers > 1:
+        if kwargs.get("num_workers", 1) > 1:
             from torch.multiprocessing import set_start_method
 
             set_start_method("spawn", force=True)
         return locals()[name](**kwargs)
     except KeyError:
         raise ValueError(f"Dataset '{name}' not found.")
+
+
+def _dataset(
+    blurs,
+    num_objects=1,
+    train_range=(0, 0.9),
+    valid_range=(0.9, 1),
+    num_workers=1,
+):
+    return DataModule(
+        train_data=MultiDataset(
+            FmoLoader("data/fmo_3_24_v1.zip", item_range=train_range, blurs=blurs),
+            BackgroundAdder(
+                ZipLoader("data/vot2018.zip", balance_subdirs=True),
+            ),
+            num_objects=num_objects,
+        ),
+        valid_data=MultiDataset(
+            FmoLoader("data/fmo_3_24_v1.zip", item_range=valid_range, blurs=blurs),
+            BackgroundAdder(
+                ZipLoader("data/otb.zip", filter="*.jpg", balance_subdirs=True),
+            ),
+            num_objects=num_objects,
+        ),
+        predict_data=MultiDataset(
+            FmoLoader(
+                "data/falling.zip", dummy_frames=24, blurs=[x - 1 for x in blurs]
+            ),
+            background_adder=None,
+        ),
+        num_workers=num_workers,
+    )
